ed81b790c885ab0bca49c5ff35baa63c6174755b
MAYANK YADAV <mayankrao2018@users.noreply.github.com>
2025-04-29 23:56:24 +0530

    Fix dynamic import for QRCodeCanvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
-const QRCode = dynamic(() => import('qrcode.react'), { ssr: false });
+const QRCodeCanvas = dynamic(() =>
+  import('qrcode.react').then((mod) => mod.QRCodeCanvas),
+  { ssr: false }
+);
 
 export default function QRPage() {
   const [url, setUrl] = useState('');
@@ -14,21 +17,8 @@ export default function QRPage() {
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white p-4">
-      <h1 className="text-2xl font-bold mb-4">Scan to View Menu</h1>
-
-      {url && (
-        <div className="bg-gray-100 p-6 rounded-xl shadow-md">
-          <QRCode value={url} size={256} />
-        </div>
-      )}
-
-      <p className="mt-4 text-sm text-gray-600">
-        For developer access:&nbsp;
-        <Link href="/home" className="text-blue-600 underline">
-          Go to Home Page
-        </Link>
-      </p>
+    <div>
+      <QRCodeCanvas value={url} />
     </div>
   );
 }
